392524a68b170ad8eb57204714d19741c603876f
Venciki <venciki@VencikideMacBook-Pro.local>
2025-03-31 16:58:36 +0800

    add new project

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,64 @@
+module.exports = {
+    title: 'XXX的博客',
+    description: 'YYY',
+    theme: 'reco',
+    base: '/venciki.github.io/',
+    themeConfig: {
+        type: "blog",
+        logo: "/naruto.jpeg",
+        authorAvatar: "/naruto.jpeg",
+        nav: [
+            { text: "首页", link: "/" },
+            {
+                text: "Venciki Blog",
+                items: [
+                    { text: "掘金", link: "https://juejin.cn" },
+                    { text: "Github", link: "https://github.com" }
+                ]
+            }
+        ],
+        // sidebar: [
+        //     {
+        //       title: "欢迎学习",
+        //       path: "/",
+        //       collapsable: false,  // 是否折叠
+        //       children: [{ title: "博客简介", path: "/" }],
+        //     },
+        //     {
+        //       title: "基础篇",
+        //       path: "/blogs/1",
+        //       collapsable: true,
+        //       children: [
+        //         { title: "第一篇", path: "/blogs/1" },
+        //         { title: "第二篇", path: "/blogs/2" },
+        //       ]
+        //     } 
+        // ]
+        author: "Venciki",
+        blogConfig: {
+            category: {
+              location: 2, // 在导航栏菜单中所占的位置，默认2
+              text: "博客", // 默认文案 “分类”
+            },
+            tag: {
+              location: 4, // 在导航栏菜单中所占的位置，默认4
+              text: "Tag", // 默认文案 “标签”
+            },
+        },
+        locales: {
+            "/": {
+              lang: "zh-CN",
+            },
+        },
+    },
+    plugins: [
+        [
+          "cursor-effects",
+          {
+            size: 4, // size of the particle, default: 2
+            shape: "star", // ['star' | 'circle'], // shape of the particle, default: 'star'
+            zIndex: 999999999, // z-index property of the canvas, default: 999999999
+          },
+        ],
+     ]
+}
---
+++ docs/README.md
@@ -0,0 +1,14 @@
+---
+home: true
+heroImage: "/naruto.jpeg"
+heroText: Venciki
+heroImageStyle:
+  {
+    maxHeight: "288px",
+    display: block,
+    borderRadius: "19% 81% 23% 77% / 44% 57% 43% 56%",
+    boxShadow: "0 15px 18px rgba(0,0,0,0.2)",
+    # bgImage: "/blue.jpg",
+    # bgImageStyle: { height: "100%", boxShadow: "0 15px 18px rgba(0,0,0,0.2)" }
+  } 
+---
